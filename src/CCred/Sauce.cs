70a42f81e1ed7fab1595ccd65c55d9efdd7a6703
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-18 23:39:18 +0700

    add salt generator

--- src/CCred/Sauce.cs
+++ src/CCred/Sauce.cs
@@ -1,7 +1,38 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace CCred
 {
-    public static class Sauce
+    public class Sauce
     {
+        public static string GenerateSalt(int length)
+        {
+            if(length <= 0)
+            {
+                throw new InvalidOperationException();
+            }
 
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+            string numeric = "0123456789";
+            var rng = new Random();
+            char[] salt = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                if(rng.Next(2) < 1)
+                {
+                    salt[i] = alphabet[rng.Next(alphabet.Length - 1)];
+                    salt[i] = rng.Next(2) > 0
+                        ? salt[i] = Char.ToLower(salt[i])
+                        : salt[i];
+                    continue;
+                }
+
+                salt[i] = numeric[rng.Next(numeric.Length - 1)];
+            }
+
+            return new string(salt);
+        }
     }
 }
