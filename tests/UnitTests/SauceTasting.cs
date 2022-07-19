efa36894963907161d6768d870b9fbca6f79522d
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-19 13:36:51 +0700

    tests

--- tests/UnitTests/SauceTasting.cs
+++ tests/UnitTests/SauceTasting.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace UnitTests;
 
 public class SauceTasting
@@ -23,5 +26,53 @@ public class SauceTasting
         var hashDefault = CCred.Sauce.GetHash(_password, _salt);
 
         Assert.True(CCred.Sauce.Verify(password, _salt, hashDefault));
+        Assert.True(CCred.Sauce.Verify<SHA384>(password, _salt, hashDefault));
+    }
+
+    [Theory]
+    [InlineData("VeryLongPassword")]
+    public void SHA258_Tests(string password)
+    {
+        var hash256 = CCred.Sauce.GetHash<SHA256>(_password, _salt);
+        var hash384 = CCred.Sauce.GetHash<SHA384>(_password, _salt);
+        var hash512 = CCred.Sauce.GetHash<SHA512>(_password, _salt);
+
+        Assert.True(CCred.Sauce.Verify<SHA256>(password, _salt, hash256));
+        Assert.False(CCred.Sauce.Verify<SHA256>(password, _salt, hash384));
+        Assert.False(CCred.Sauce.Verify<SHA256>(password, _salt, hash512));
+    }
+
+    [Theory]
+    [InlineData("VeryLongPassword")]
+    public void SHA384_Tests(string password)
+    {
+        var hash256 = CCred.Sauce.GetHash<SHA256>(_password, _salt);
+        var hash384 = CCred.Sauce.GetHash<SHA384>(_password, _salt);
+        var hash512 = CCred.Sauce.GetHash<SHA512>(_password, _salt);
+
+        Assert.False(CCred.Sauce.Verify<SHA384>(password, _salt, hash256));
+        Assert.True(CCred.Sauce.Verify<SHA384>(password, _salt, hash384));
+        Assert.False(CCred.Sauce.Verify<SHA384>(password, _salt, hash512));
+    }
+
+    [Theory]
+    [InlineData("VeryLongPassword")]
+    public void SHA512_Tests(string password)
+    {
+        var hash256 = CCred.Sauce.GetHash<SHA256>(_password, _salt);
+        var hash384 = CCred.Sauce.GetHash<SHA384>(_password, _salt);
+        var hash512 = CCred.Sauce.GetHash<SHA512>(_password, _salt);
+
+        Assert.False(CCred.Sauce.Verify<SHA512>(password, _salt, hash256));
+        Assert.False(CCred.Sauce.Verify<SHA512>(password, _salt, hash384));
+        Assert.True(CCred.Sauce.Verify<SHA512>(password, _salt, hash512));
+    }
+
+    [Theory]
+    [InlineData("VeryLongPassword")]
+    public void EncodingTest(string password)
+    {
+        var hash384 = CCred.Sauce.GetHash(_password, _salt, Encoding.UTF32);
+        Assert.False(CCred.Sauce.Verify(password, _salt, hash384));
     }
 }
